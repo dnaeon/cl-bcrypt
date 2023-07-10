1d4e023dd12a49b086f15bd5e1cb8c28a14a2476
Marin Atanasov Nikolov <dnaeon@gmail.com>
2023-07-10 11:35:43 +0300

    Update test pipelines
    Disable debugger during CI tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,7 +4,18 @@ set -e
 
 LISP=${LISP:-sbcl}
 
-${LISP} --eval '(ql:quickload :cl-bcrypt.test)' \
+_no_debug_flag=""
+case "${LISP}" in
+    sbcl)
+        _no_debug_flag="--non-interactive"
+        ;;
+    ecl)
+        _no_debug_flag="--nodebug"
+        ;;
+esac
+
+${LISP} ${_no_debug_flag} \
+        --eval '(ql:quickload :cl-bcrypt.test)' \
         --eval '(setf rove:*enable-colors* nil)' \
         --eval '(asdf:test-system :cl-bcrypt.test)' \
         --eval '(uiop:quit (length (rove/core/stats:all-failed-assertions rove/core/stats:*stats*)))'
